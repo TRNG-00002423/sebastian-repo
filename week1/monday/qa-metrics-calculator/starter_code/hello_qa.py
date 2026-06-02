62b0b04ab8d1fee548dbf0439b1381582900084b
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2026-06-02 13:32:57 -0500

    Finished task 1

---
+++ week1/monday/qa-metrics-calculator/starter_code/hello_qa.py
@@ -0,0 +1,6 @@
+import sys
+
+name = input("What is your name? ")
+role = input("What is your role? ")
+print(f"Welcome, {name}! Your role is {role}.")
+print(f"Python version: {sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}")
