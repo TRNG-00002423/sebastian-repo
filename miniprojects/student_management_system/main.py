8f7d56bb48881d07047bc8142c4623d83de1eb87
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2026-06-08 22:27:40 -0500

    removed student object

--- miniprojects/student_management_system/main.py
+++ miniprojects/student_management_system/main.py
@@ -1,14 +1,19 @@
 from __future__ import annotations
 from pathlib import Path
 from flask import Flask, jsonify, request
-from student import Student
 import json
 
 app = Flask("Student Management System ")
 
+def db_size():
+    with open('db.json', 'r') as f:
+        json_file = json.load(f)        
+    return len(json_file)
+
 def db_read_all():
     with open('db.json', 'r') as f:
         json_file = json.load(f)
+        print(len(json_file))
     return json_file
 
 
@@ -18,17 +23,17 @@ def db_read(student_id:int):
     student = list(filter(lambda x: int(x['id']) == int(student_id), json_file))
     return student
 
-def db_write(student:Student):
+def db_write(student):
     with open('db.json', 'r') as f:
         db_json = json.load(f)
-
-    db_json.append(student.to_dict())
+    student['id'] = db_size()
+    db_json.append(student)
 
     with open('db.json', 'w') as f:
         json.dump(db_json, f, indent=4)
     
 
-    return student.to_dict()
+    return student
 
 @app.get("/student/")
 def get_all_student(): 
@@ -41,12 +46,10 @@ def get_student(student_id:int):
 @app.post("/student/")
 def post_student():
     data = request.get_json()
-    student = Student(
-        data["id"],
-        data["name"],
-        data["course"]
-    )   
-    
+    student = {'id':0,
+        'name':data["name"],
+        'course':data["course"]
+    }    
     return jsonify(db_write(student))
 
 
